4dc3e3118601b385baccba0b0a50c98d4ec0998e
Andrea Wieland <13319601+AndreaWieland@users.noreply.github.com>
2018-07-24 21:08:34 -0700

    commented method workings

--- src/API/Controllers/AlertController.php
+++ src/API/Controllers/AlertController.php
@@ -12,12 +12,16 @@
         public function setAlert($price, $id, $email)
         {
             //create alert
+            //if price < 0, return false
+            //if id not valid, return false
+            //if email format not valid, return false
             
         }
         
         public function sendAlert()
         {
             //send message to email
+            //if message send fails, return false
             
         }
     
