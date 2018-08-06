c0c0b132bf2d28b0859a6b705572cb903ac0891b
Brandon Lewis <branlewis@csumb.edu>
2018-08-06 19:00:05 -0400

    Complete AlertController with dependency injection pattern

--- src/API/Controllers/AlertController.php
+++ src/API/Controllers/AlertController.php
@@ -1,30 +1,74 @@
 <?php
-    
-    require(dirname(__FILE__).'/'.'BaseController.php'); 
-    
+
+    require_once($_SERVER['DOCUMENT_ROOT'] . 'src/API/Controllers/BaseController.php');
+    require_once($_SERVER['DOCUMENT_ROOT'] . 'src/API/Controllers/Database.php');
+
     class AlertController extends BaseController{
-    
-        private function __construct($price, $id, $email)
+
+        protected $db;
+
+        // Dependency Injection
+        public function __construct(Database $db)
         {
-            $this->setAlert($price, $id, $email);
+            $this->db = $db;
         }
-        
+
         public function setAlert($price, $id, $email)
         {
-            //create alert
-            //if price < 0, return false
-            //if id not valid, return false
-            //if email format not valid, return false
-            
+            // Create alert
+            // If price < 0, return false
+            if($price < 0)
+            {
+                echo("$price is not a valid value");
+                return false;
+            }
+
+            // If id not valid, return false
+            if(!in_array($id, $ids))
+            {
+                echo("$id not in $ids");
+                return false;
+            }
+
+            // If email format not valid, return false
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+            {
+                echo("$email is not a valid email address");
+                return false;
+            }
+
+            // Inserts email into db with corresponding data
+            $this->db->query("INSERT INTO Alerts (price, id, email)
+                VALUES ({$price}, {$id}, {$email}) ");
+
         }
-        
-        public function sendAlert()
+
+        public function sendAlert($price, $id)
         {
-            //send message to email
-            //if message send fails, return false
-            
+            // send message to email
+
+            // Dependency Injection
+            $result = $this->db->query("SELECT email FROM Alerts WHERE id = {$id} AND price <= {$price}");
+            $message = "Coin {$is} has reached \${$price}";
+
+            // Sends to each email that matches query
+            // If message send fails, return false
+            while($row = mysql_fetch_array($result))
+            {
+                // send email
+                $mail_sent = mail($row['email'],"Coin Allert",$message);
+                if ($mail_sent)
+                {
+                    echo "Mail Sent";
+                } else {
+                    echo "Mail Failed";
+                    return false;
+                }
+
+            }
+
         }
-    
+
     }
-    
+
 ?>
--- tests/ControllerTests/AlertControllerTest.php
+++ tests/ControllerTests/AlertControllerTest.php
@@ -3,36 +3,58 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
+require_once($_SERVER['DOCUMENT_ROOT'] . 'src/API/Controllers/AlertController.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . 'src/API/Controllers/Database.php');
+
 final class AlertControllerTest extends TestCase
 {
-  
-    public function setValidAlert(){
+    public function setUp()
+    {
+        // Setting up a dependency injected AlertController
+        $this->db = new Database();
+        $this->ac = new AlertController($this->db);
+    }
+
+    public function tearDown()
+    {
+        // tear down user AlertController
+        unset($this->ac);
+    }
+
+    // Test that a valid alert sets and returns true
+    public function testSetValidAlert(){
         $this->assertNotFalse(
-            AlertController::setAlert(1.25, 1182, 'awieland@csumb.edu')
+            $this->ac->setAlert(1.25, 1182, 'awieland@csumb.edu')
         );
     }
-  
-    public function setInvalidIdAlert(){
+
+    // Tests that an invalid attempt at setAlert return false
+    public function testSetInvalidIdAlert(){
         $this->assertFalse(
-            AlertController::setAlert(1.25, 0, 'awieland@csumb.edu')
+            $this->ac->setAlert(1.25, 0, 'awieland@csumb.edu')
         );
     }
-  
-    public function setInvalidPriceAlert(){
+
+    // Tests that an invalid price will return false
+    public function testSetInvalidPriceAlert(){
         $this->assertFalse(
-            AlertController::setAlert(-0.1, 1182, 'awieland@csumb.edu')
+            $this->ac->setAlert(-0.1, 1182, 'awieland@csumb.edu')
         );
     }
-    
-    public function setInvalidEmailAlert(){
+
+    // Tests that an invalid email will return false
+    public function testSetInvalidEmailAlert(){
         $this->assertFalse(
-            AlertController::setAlert(1.25, 1182, 'Andrea')
+            $this->ac->setAlert(1.25, 1182, 'Andrea')
         );
     }
-  
-    public function sendAlertValidEmail(){
+
+    // Tests that email sending works with valid input
+    public function testSendAlertValidEmail(){
         $this->assertNotFalse(
-            AlertController::sendAlert()
+            $this->ac->sendAlert(1.25, 1182)
         );
     }
 }
+
+?>
